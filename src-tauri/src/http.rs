d2e4f935aa42c5f9af67797670980301ef35d3a3
imguolao <guolao.mq@foxmail.com>
2024-01-04 20:20:22 +0800

    init

---
+++ src-tauri/src/http.rs
@@ -0,0 +1,12 @@
+use reqwest::blocking::Client;
+
+pub type Error = reqwest::Error;
+
+pub type Response = reqwest::Response;
+
+pub fn get(url: &str) -> Result<Response, Error> {
+  Client::new()
+    .get(url)
+    .header("User-Agent", concat!("nvm_rs_desktop", env!("CARGO_PKG_VERSION")))
+    .send()
+}
